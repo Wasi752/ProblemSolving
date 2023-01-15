ed505f01d5b4a55eaca548143b110fc8284353d4
Wasi752 <113251445+Wasi752@users.noreply.github.com>
2023-01-15 21:14:09 +0600

    Create sol-5.py
    BoX

---
+++ sol-5.py
@@ -0,0 +1,14 @@
+t = int(input())
+i = 0
+while t > i:
+    x = int(input())
+    j = 0
+    while j < x:
+        k = 0
+        while k < x:
+            print("*", end='')
+            k = k + 1
+        print("")
+        j = j + 1
+    print('')
+    i = i + 1
